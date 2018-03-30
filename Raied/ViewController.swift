fec8da7f74d8aa23af3704f8fcfec98631924fc7
meshal binnasban <mnasban@hotmail.com>
2018-03-30 15:30:54 +0300

    Initial Commit

---
+++ Raied/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Raied
+//
+//  Created by meshal binnasban on 13/07/1439 AH.
+//  Copyright © 1439 meshal binnasban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
